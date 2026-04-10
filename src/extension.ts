9c8129579a0bedd270f41dfb341f491e4ab092c1
TengraStudio <market@tengra.ai>
2026-04-10 20:04:42 +0300

    initial commit: job finder plugin base architecture

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+/**
+ * Job Finder Extension for Tengra
+ */
+
+export async function activate(context: any) {
+  context.logger.info('Job Finder Extension Activated');
+  
+  // Registration logic will go here
+  // For now, it just confirms activation
+}
+
+export async function deactivate() {
+  // Cleanup logic
+}
